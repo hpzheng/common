84831e2b443e55c4bd99469f78cc05debf7d3d8e
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-08-10 08:57:53 -0400

    Taking list of sf from unzipped dir

--- scripts/pdb_data/utils.py
+++ scripts/pdb_data/utils.py
@@ -2,14 +2,8 @@ import config
 from glob import iglob
 import os
 
-structure_factors_path = os.path.join(config.pdb_repository_dir,
-                                      'data', 
-                                      'structures',
-                                      'all',
-                                      'structure_factors'
-                                      )
 def codes_with_structure_factors():
-    for fn in iglob(os.path.join(structure_factors_path, '*')):
+    for fn in iglob(os.path.join(config.structure_factors_path, '*')):
         code = os.path.basename(fn).split('.')[0][1:5]
         yield code
 
